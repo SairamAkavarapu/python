2ace26dfd8a740d2259ee096b2d5a119be9b1987
SairamAkavarapu <163122488+SairamAkavarapu@users.noreply.github.com>
2024-03-26 09:47:03 +0530

    Add files via upload

--- Checking whether the given year is a leap year or not.py
+++ Checking whether the given year is a leap year or not.py
@@ -1,7 +1,5 @@
 year=int(input("Enter any one year:"))
-if(year%4==0 and year%100!=0):
-    print(year,"is a leap year")
-elif(year%100==0 and year%400==0):
+if(year%4==0 and year%100!=0 or year%100==0 and year%400==0):
     print(year,"is a leap year")
 else:
     print(year,"is not a leap year")
